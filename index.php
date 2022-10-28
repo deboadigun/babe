b85bdd96c672d76fbcf1cf129be71cf6754c032f
deboadigun <deboadigun@gmail.com>
2022-10-28 02:24:27 -0400

    babe

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 	<link rel="hortcut icon" href="ico/sayfa.ico" />
-    <title>Panel</title>
+    
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
